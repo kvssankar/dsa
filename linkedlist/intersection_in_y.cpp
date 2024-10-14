3ee08ba2d08fa9e8425f239f6a4395dbb949d01b
Kvs Sankar Kumar <55633494+kvssankar@users.noreply.github.com>
2024-10-14 14:30:41 +0530

    done

--- linkedlist/intersection_in_y.cpp
+++ linkedlist/intersection_in_y.cpp
@@ -1,48 +1,88 @@
-int intersectPoint(Node* head1, Node* head2)
-{
-    Node *ptr1=head1;
-    Node *ptr2=head2;
-    if(head1==NULL || head2==NULL)
-    return -1;
-    int count1=1,count2=1;
-    while(ptr1->next!=NULL){
-        count1++;
-        ptr1=ptr1->next;
-    }
-    while(ptr2->next!=NULL){
-        count2++;
-        ptr2=ptr2->next;
-    }
-    ptr1=head1;
-    ptr2=head2;
-    int d=abs(count1-count2);
-    if(count1>count2){
-        for(int i=0;i<d;i++){
-            if(ptr1==NULL)
-            return -1;
-            ptr1=ptr1->next;
-        }
-        while(ptr1!=NULL && ptr2!=NULL){
-            if(ptr1==ptr2){
-                return ptr1->data;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int difference(ListNode *headA, ListNode *headB){
+        int a=0,b=0;
+        while(headA!=NULL || headB!=NULL){
+            if(headA){
+                a++;
+                headA=headA->next;
             }
-            ptr1=ptr1->next;
-            ptr2=ptr2->next;
-        }
-        return -1;
-    }else{
-        for(int i=0;i<d;i++){
-            if(ptr2==NULL)
-            return -1;
-            ptr2=ptr2->next;
-        }
-        while(ptr1!=NULL && ptr2!=NULL){
-            if(ptr1==ptr2){
-                return ptr1->data;
+            if(headB){
+                b++;
+                headB=headB->next;
             }
-            ptr1=ptr1->next;
-            ptr2=ptr2->next;
         }
-        return -1;
+        return a-b;
     }
-}
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int diff =  difference(headA,headB);
+        if(diff>0){
+            while(diff-- >0) headA=headA->next;
+        }
+        else{
+            while(diff++ <0) headB=headB->next;
+        }
+        while(headA && headB && headA!=headB){
+            headA=headA->next;
+            headB=headB->next;
+        }
+        return headA;
+    }
+};
+
+// int intersectPoint(Node* head1, Node* head2)
+// {
+//     Node *ptr1=head1;
+//     Node *ptr2=head2;
+//     if(head1==NULL || head2==NULL)
+//     return -1;
+//     int count1=1,count2=1;
+//     while(ptr1->next!=NULL){
+//         count1++;
+//         ptr1=ptr1->next;
+//     }
+//     while(ptr2->next!=NULL){
+//         count2++;
+//         ptr2=ptr2->next;
+//     }
+//     ptr1=head1;
+//     ptr2=head2;
+//     int d=abs(count1-count2);
+//     if(count1>count2){
+//         for(int i=0;i<d;i++){
+//             if(ptr1==NULL)
+//             return -1;
+//             ptr1=ptr1->next;
+//         }
+//         while(ptr1!=NULL && ptr2!=NULL){
+//             if(ptr1==ptr2){
+//                 return ptr1->data;
+//             }
+//             ptr1=ptr1->next;
+//             ptr2=ptr2->next;
+//         }
+//         return -1;
+//     }else{
+//         for(int i=0;i<d;i++){
+//             if(ptr2==NULL)
+//             return -1;
+//             ptr2=ptr2->next;
+//         }
+//         while(ptr1!=NULL && ptr2!=NULL){
+//             if(ptr1==ptr2){
+//                 return ptr1->data;
+//             }
+//             ptr1=ptr1->next;
+//             ptr2=ptr2->next;
+//         }
+//         return -1;
+//     }
+// }
