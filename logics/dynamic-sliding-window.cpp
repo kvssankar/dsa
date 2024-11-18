c6c77cfed09dfdabb73172024b8585c36b01519b
Kvs Sankar Kumar <55633494+kvssankar@users.noreply.github.com>
2024-11-18 13:02:10 +0530

    done

--- logics/dynamic-sliding-window.cpp
+++ logics/dynamic-sliding-window.cpp
@@ -1,6 +1,3 @@
-// Given an array of positive integers nums and a positive integer target, return the minimal length of a 
-// subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
-
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
@@ -8,20 +5,16 @@ public:
         int l=0,r=0;
         int n=nums.size();
         int ans=INT_MAX;
-        while(l<n){
-            if(r<n && sum<target){
-                sum+=nums[r];
-                r++;
-            }else if(sum>=target){ //here r can be greter than end which handles the last case
-                ans=min(ans,r-l);
+        for(r=0;r<n;r++){
+            sum+=nums[r];
+            while(l<=r && sum>=target){
+                ans=min(ans,r-l+1);
                 sum-=nums[l];
                 l++;
-            }else{
-                // current sum is less than num, BUT window's leading edge HAS reached end of array
-                // needed to prevent an infinite loop 
-                break;
             }
         }
         return ans==INT_MAX ? 0 : ans;
     }
 };
+
+//https://leetcode.com/problems/max-consecutive-ones-iii/submissions/1456006091/?envType=study-plan-v2&envId=leetcode-75
