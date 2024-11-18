cfa3ee47ea81a1a1a7181f253e2190b40832c1c2
Kvs Sankar Kumar <55633494+kvssankar@users.noreply.github.com>
2024-11-18 11:30:40 +0530

    done

---
+++ logics/dynamic-sliding-window.cpp
@@ -0,0 +1,27 @@
+// Given an array of positive integers nums and a positive integer target, return the minimal length of a 
+// subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int sum=0;
+        int l=0,r=0;
+        int n=nums.size();
+        int ans=INT_MAX;
+        while(l<n){
+            if(r<n && sum<target){
+                sum+=nums[r];
+                r++;
+            }else if(sum>=target){
+                ans=min(ans,r-l);
+                sum-=nums[l];
+                l++;
+            }else{
+                // current sum is less than num, BUT window's leading edge HAS reached end of array
+                // needed to prevent an infinite loop 
+                break;
+            }
+        }
+        return ans==INT_MAX ? 0 : ans;
+    }
+};
