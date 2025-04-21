e26a625535f5b3a0afaff53ccaef7662e70414fb
Kvs Sankar Kumar <55633494+kvssankar@users.noreply.github.com>
2025-04-21 20:19:38 +0530

    s

--- README.md
+++ README.md
@@ -6,3 +6,19 @@ https://leetcode.com/studyplan/leetcode-75/
 int idx = lower_bound(vec.begin(), vec.end(), qi) - vec.begin();
 int nextIdx = (idx + 1) % sz;
 int prevIdx = (idx - 1 + sz) % sz;©leetcode
+
+ // Set with greatest elements on top (descending order)
+set<int, greater<int>> s;
+
+#include <iostream>
+#include <set>
+#include <numeric> // for accumulate
+using namespace std;
+
+int main() {
+    set<int> s = {1, 2, 3, 4, 5};
+    int sum = accumulate(s.begin(), s.end(), 0);
+
+    cout << "Sum: " << sum << endl;
+    return 0;
+}
--- heap/basic-k largest element.cpp
+++ heap/basic-k largest element.cpp
@@ -13,6 +13,26 @@ public:
         return pq.top();
     }
 };
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int k = 3;
+priority_queue<int, vector<int>, greater<int>> pq; // min-heap
+
+void push(int x) {
+    pq.push(x);
+    if (pq.size() > k) pq.pop(); // remove smallest if size exceeds k
+}
+
+void printTopKUnsorted() {
+    priority_queue<int, vector<int>, greater<int>> temp = pq;
+    while (!temp.empty()) {
+        cout << temp.top() << " ";
+        temp.pop();
+    }
+    cout << endl;
+}
 
 //kth smallest
 class Solution {
@@ -29,3 +49,23 @@ public:
         return pq.top();
     }
 };
+#include <iostream>
+#include <set>
+using namespace std;
+
+int k = 3;
+multiset<int> topKSmallest;  // stores k smallest elements
+
+void push(int x) {
+    topKSmallest.insert(x);
+    if (topKSmallest.size() > k) {
+        auto it = prev(topKSmallest.end()); // erase largest
+        topKSmallest.erase(it);
+    }
+}
+
+void printTopKSmallest() {
+    for (int val : topKSmallest)
+        cout << val << " ";
+    cout << endl;
+}
