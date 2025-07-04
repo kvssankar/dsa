b3e77df29de0ba878e684dd9488e999985b8c7c1
Kvs Sankar Kumar <55633494+kvssankar@users.noreply.github.com>
2025-07-04 16:47:59 +0530

    d

--- binarysearch/pivot.cpp
+++ binarysearch/pivot.cpp
@@ -2,6 +2,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//this can find minmum only it cant find pivot for duplicates
+class Solution {
+public:
+    int findMin(vector<int>& arr) {
+        int n=arr.size();
+        int l=0;
+        int r=n-1;
+        int ans=INT_MAX;
+        while(l<=r){
+            int m=l+(r-l)/2;
+            if(arr[l]<=arr[m]){
+                ans=min(ans,arr[l]);
+                l=m+1;
+            }else{
+                ans = min(ans, arr[m]);
+                r=m-1;
+            }
+        }
+        return ans;
+    }
+};
+
+
 bool searchInARotatedSortedArrayII(vector<int>&arr, int k) {
     int n = arr.size(); // size of the array.
     int low = 0, high = n - 1;
