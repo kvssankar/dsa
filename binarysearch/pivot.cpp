e06a64057ebe744a96e0725084afe113118e28cc
Kvs Sankar Kumar <55633494+kvssankar@users.noreply.github.com>
2025-07-04 16:44:38 +0530

    done

--- binarysearch/pivot.cpp
+++ binarysearch/pivot.cpp
@@ -1,36 +1,57 @@
-// 3 5 1 2 3
 
-int l=0;
-int r=n-1;
-//wont work for dups
-while(l<=r){
-    int m=l+(r-l)/2;
-    int next=(m+1)%n;
-    int prev=(m-1+n)%n;
-    if(arr[m]<=arr[next] && arr[m]<=arr[prev]){
-        return mid;
-    }
-    if(arr[l]<=arr[m]){
-        l=m+1;
-    }
-    else{
-        r=m-1;
-    }
-}
+#include <bits/stdc++.h>
+using namespace std;
 
+bool searchInARotatedSortedArrayII(vector<int>&arr, int k) {
+    int n = arr.size(); // size of the array.
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
 
+        //if mid points the target
+        if (arr[mid] == k) return true;
 
- int n  = static_cast<int>(nums.size());
-        int lo = 0, hi = n - 1;
+        //Edge case:
+        if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
+            low = low + 1;
+            high = high - 1;
+            continue;
+        }
 
-        int pivotCandidate = lo;               // some minimum value
-
-        /* 2️⃣  Ensure we pick the *first* minimum that follows the drop.
-               (Handles “… 2, 1, 1, 1 …” where the first min sits at index 0.)   */
-        for (int i = 1; i < n; ++i) {
-            if (nums[i] < nums[i - 1]) {       // real rotation point
-                pivotCandidate = i;
-                break;
+        //if left part is sorted:
+        if (arr[low] <= arr[mid]) {
+            if (arr[low] <= k && k <= arr[mid]) {
+                //element exists:
+                high = mid - 1;
+            }
+            else {
+                //element does not exist:
+                low = mid + 1;
             }
         }
-        return pivotCandidate;     
+        else { //if right part is sorted:
+            if (arr[mid] <= k && k <= arr[high]) {
+                //element exists:
+                low = mid + 1;
+            }
+            else {
+                //element does not exist:
+                high = mid - 1;
+            }
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    vector<int> arr = {7, 8, 1, 2, 3, 3, 3, 4, 5, 6};
+    int k = 3;
+    bool ans = searchInARotatedSortedArrayII(arr, k);
+    if (!ans)
+        cout << "Target is not present.\n";
+    else
+        cout << "Target is present in the array.\n";
+    return 0;
+}
+
