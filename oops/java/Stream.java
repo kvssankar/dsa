8850393e47b705d4cf9be3846bf07655e9cd79eb
Kvs Sankar Kumar <55633494+kvssankar@users.noreply.github.com>
2025-08-01 19:00:08 +0530

    fas

--- oops/java/Stream.java
+++ oops/java/Stream.java
@@ -26,3 +26,61 @@ public class Stream {
         sol.printList(al);
     }
 }
+// java.lang.Comparable<T>
+class Order implements Comparable<Order> {
+    int mid, sid, price;
+
+    public Order(int mid, int sid, int price) {
+        this.mid = mid;
+        this.sid = sid;
+        this.price = price;
+    }
+
+    @Override
+    public int compareTo(Order o) {
+        // Example: sort by price, then mid, then sid
+        if (this.price != o.price)
+            return Integer.compare(this.price, o.price);
+        if (this.mid != o.mid)
+            return Integer.compare(this.mid, o.mid);
+        return Integer.compare(this.sid, o.sid);
+    }
+}
+
+
+//comparator
+class Order {
+    int mid, sid, price;
+
+    public Order(int mid, int sid, int price) {
+        this.mid = mid;
+        this.sid = sid;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + mid + ", " + sid + ", " + price + ")";
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Comparator<Order> cmp = (a, b) -> {
+            if (a.price != b.price) return Integer.compare(a.price, b.price);
+            if (a.mid != b.mid) return Integer.compare(a.mid, b.mid);
+            return Integer.compare(a.sid, b.sid);
+        };
+
+        TreeSet<Order> set = new TreeSet<>(cmp);
+
+        set.add(new Order(1, 2, 100));
+        set.add(new Order(1, 2, 100)); // duplicate
+        set.add(new Order(2, 1, 90));
+        set.add(new Order(1, 3, 100));
+
+        for (Order o : set) {
+            System.out.println(o);
+        }
+    }
+}
